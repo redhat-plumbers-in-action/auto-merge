1cb4af12ee0fcf1ff20949764775c50130f4b1bc
Jan Macku <jamacku@redhat.com>
2025-10-01 09:50:44 +0200

    deps: update dependencies

--- dist/schema/pull-request.d.ts
+++ dist/schema/pull-request.d.ts
@@ -2,49 +2,19 @@ import { z } from 'zod';
 export declare const pullRequestApiSchema: z.ZodObject<{
     number: z.ZodNumber;
     title: z.ZodString;
-    base: z.ZodEffects<z.ZodObject<{
+    base: z.ZodPipe<z.ZodObject<{
         ref: z.ZodString;
-    }, "strip", z.ZodTypeAny, {
+    }, z.core.$strip>, z.ZodTransform<string, {
         ref: string;
-    }, {
-        ref: string;
-    }>, string, {
-        ref: string;
-    }>;
-    labels: z.ZodArray<z.ZodEffects<z.ZodObject<{
+    }>>;
+    labels: z.ZodArray<z.ZodPipe<z.ZodObject<{
         name: z.ZodString;
-    }, "strip", z.ZodTypeAny, {
+    }, z.core.$strip>, z.ZodTransform<string, {
         name: string;
-    }, {
-        name: string;
-    }>, string, {
-        name: string;
-    }>, "many">;
+    }>>>;
     draft: z.ZodOptional<z.ZodBoolean>;
     merged: z.ZodBoolean;
     mergeable: z.ZodNullable<z.ZodBoolean>;
     mergeable_state: z.ZodString;
-}, "strip", z.ZodTypeAny, {
-    number: number;
-    base: string;
-    labels: string[];
-    title: string;
-    merged: boolean;
-    mergeable: boolean | null;
-    mergeable_state: string;
-    draft?: boolean | undefined;
-}, {
-    number: number;
-    base: {
-        ref: string;
-    };
-    labels: {
-        name: string;
-    }[];
-    title: string;
-    merged: boolean;
-    mergeable: boolean | null;
-    mergeable_state: string;
-    draft?: boolean | undefined;
-}>;
+}, z.core.$strip>;
 export type PullRequestApi = z.infer<typeof pullRequestApiSchema>;
