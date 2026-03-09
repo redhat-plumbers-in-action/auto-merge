c1e689e8dd4e8d1fd44f1ab487db2ee9551d0f64
Jan Macku <jamacku@redhat.com>
2026-03-09 12:25:19 +0100

    build: migrate to esbuild

--- dist/error.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class AutoMergeError extends Error {
-    readonly code?: number | undefined;
-    constructor(message: string, code?: number | undefined);
-}
--- dist/error.js
+++
@@ -1,7 +0,0 @@
-export class AutoMergeError extends Error {
-    constructor(message, code) {
-        super(message);
-        this.code = code;
-    }
-}
-//# sourceMappingURL=error.js.map
